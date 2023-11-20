e879021c3cfd22006fc9c778e7b628683b022e95
Brian Landes <the.brian.landes@gmail.com>
2023-11-20 14:52:30 -0700

    Filter on GetInstancesNode

--- Assets/Narramancer/Scripts/Nodes/GetInstancesNode.cs
+++ Assets/Narramancer/Scripts/Nodes/GetInstancesNode.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using XNode;
 
@@ -7,16 +9,38 @@ namespace Narramancer {
 	[CreateNodeMenu("Noun/Get Instances")]
 	public class GetInstancesNode : Node {
 
+		[Serializable]
+		public enum TypeFilter {
+			All,
+			Characters,
+			Items,
+			Locations
+		}
+		[SerializeField]
+		[NodeEnum]
+		TypeFilter filter = TypeFilter.All;
+
 		[Output]
 		[SerializeField]
 		[HideLabel]
 		private List<NounInstance> instances = default;
 
-		// TODO: filter by NounType
-
 		public override object GetValue(object context, NodePort port) {
 			if (Application.isPlaying && port.fieldName.Equals(nameof(instances))) {
-				return NarramancerSingleton.Instance.GetInstances();
+				switch (filter) {
+					case TypeFilter.All:
+						return NarramancerSingleton.Instance.GetInstances();
+
+					case TypeFilter.Characters:
+						return NarramancerSingleton.Instance.GetInstances().Where(x=>x.NounType == NounType.Character).ToList();
+
+					case TypeFilter.Items:
+						return NarramancerSingleton.Instance.GetInstances().Where(x => x.NounType == NounType.Item).ToList();
+
+					case TypeFilter.Locations:
+						return NarramancerSingleton.Instance.GetInstances().Where(x => x.NounType == NounType.Location).ToList();
+				}
+				
 			}
 			return null;
 		}
