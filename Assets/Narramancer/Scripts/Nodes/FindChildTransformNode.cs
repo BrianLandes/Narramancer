ef62eddc5ff431de3edd9c2265a27037fbbc6dd0
Brian Landes <the.brian.landes@gmail.com>
2023-11-20 10:46:56 -0700

    Improve outputs for FindChildTransformNode

--- Assets/Narramancer/Scripts/Nodes/FindChildTransformNode.cs
+++ Assets/Narramancer/Scripts/Nodes/FindChildTransformNode.cs
@@ -8,7 +8,7 @@ namespace Narramancer {
 
 		[Input(connectionType = ConnectionType.Override, typeConstraint = TypeConstraint.Inherited)]
 		[SerializeField]
-		private GameObject gameObject = default;
+		private GameObject targetGameObject = default;
 
 		[Input(connectionType = ConnectionType.Override, typeConstraint = TypeConstraint.Inherited)]
 		[SerializeField, HideLabel]
@@ -16,14 +16,27 @@ namespace Narramancer {
 
 		[Output]
 		[SerializeField]
-		private Transform objectPosition = default;
+		private Transform transform = default;
+
+		[Output]
+		[SerializeField]
+		private GameObject gameObject = default;
 
 		public override object GetValue(object context, NodePort port) {
-			if (Application.isPlaying && port.fieldName.Equals(nameof(objectPosition))) {
-				var inputGameObject = GetInputValue(context, nameof(gameObject), gameObject);
+			if (Application.isPlaying) {
+
+				var inputGameObject = GetInputValue(context, nameof(targetGameObject), targetGameObject);
 				if (inputGameObject != null) {
 					var transformName = GetInputValue(context, nameof(this.transformName), this.transformName);
-					return inputGameObject.transform.Find(transformName);
+
+					var child = inputGameObject.transform.Find(transformName);
+					switch (port.fieldName) {
+						case nameof(transform):
+							return child;
+						case nameof(gameObject):
+							return child?.gameObject;
+					}
+
 				}
 			}
 			return null;
