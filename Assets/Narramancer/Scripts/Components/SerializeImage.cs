2a633e8d9a29a6d84f5da74c8929785ba9e6dcfa
Brian Landes <the.brian.landes@gmail.com>
2023-12-18 18:10:59 -0700

    Better null handling on SerializeImage

--- Assets/Narramancer/Scripts/Components/SerializeImage.cs
+++ Assets/Narramancer/Scripts/Components/SerializeImage.cs
@@ -10,22 +10,36 @@ namespace Narramancer {
 		public override void Serialize(StoryInstance story) {
 			base.Serialize(story);
 			var image = GetComponent<Image>();
-			story.SaveTable.Set(Key("texture"), image.sprite.texture);
-			story.SaveTable.Set(Key("rect"), image.sprite.rect);
-			story.SaveTable.Set(Key("pivot"), image.sprite.pivot);
-			story.SaveTable.Set(Key("color"), image.color);
+			if (image != null) {
+				var hasSprite = image.sprite != null;
+				story.SaveTable.Set(Key("hasSprite"), hasSprite);
+				if (hasSprite) {
+					story.SaveTable.Set(Key("texture"), image.sprite.texture);
+					story.SaveTable.Set(Key("rect"), image.sprite.rect);
+					story.SaveTable.Set(Key("pivot"), image.sprite.pivot);
+				}
+				story.SaveTable.Set(Key("color"), image.color);
+			}
+
 		}
 
 		public override void Deserialize(StoryInstance map) {
 			base.Deserialize(map);
 			var image = GetComponent<Image>();
-			var texture = map.SaveTable.GetAndRemove<Texture2D>(Key("texture"));
-			var rect = map.SaveTable.GetAndRemove<Rect>(Key("rect"));
-			var pivot = map.SaveTable.GetAndRemove<Vector2>(Key("pivot"));
-			pivot /= rect.size;
-			image.sprite = Sprite.Create(texture, rect, pivot);
+			if (image != null) {
+				var hasSprite = map.SaveTable.GetAndRemove<bool>(Key("hasSprite"));
+				if (hasSprite) {
+					var texture = map.SaveTable.GetAndRemove<Texture2D>(Key("texture"));
+					var rect = map.SaveTable.GetAndRemove<Rect>(Key("rect"));
+					var pivot = map.SaveTable.GetAndRemove<Vector2>(Key("pivot"));
+					pivot /= rect.size;
+					image.sprite = Sprite.Create(texture, rect, pivot);
+				}
 
-			image.color = map.SaveTable.Get<Color>(Key("color"));
+
+				image.color = map.SaveTable.Get<Color>(Key("color"));
+			}
+
 		}
 	}
 }
