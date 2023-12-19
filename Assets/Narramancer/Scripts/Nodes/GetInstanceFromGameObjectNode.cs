7afabbfa09a9712d59468a68b1b67e79f4d53d98
Brian Landes <the.brian.landes@gmail.com>
2023-12-18 18:07:32 -0700

    Implement GetInstanceFromGameObjectNode

---
+++ Assets/Narramancer/Scripts/Nodes/GetInstanceFromGameObjectNode.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using XNode;
+
+namespace Narramancer {
+	public class GetInstanceFromGameObjectNode : Node {
+
+		[Input(ShowBackingValue.Never, ConnectionType.Override, TypeConstraint.Inherited)]
+		[SerializeField]
+		GameObject targetGameObject = default;
+
+		[Output(ShowBackingValue.Never, ConnectionType.Multiple, TypeConstraint.Inherited)]
+		[SerializeField]
+		NounInstance instance = default;
+
+		public override object GetValue(object context, NodePort port) {
+
+			if (port.fieldName.Equals(nameof(instance))) {
+				var targetGameObject = GetInputValue(context, nameof(this.targetGameObject), this.targetGameObject);
+				var nounReference = targetGameObject?.GetComponentInChildren<SerializeNounInstanceReference>();
+
+				var instance = nounReference?.GetInstance();
+				return instance;
+			}
+
+			return null;
+		}
+	}
+}
