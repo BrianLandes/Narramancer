33f21dabe9020a86ebf17acf735c62bfae1d25e7
Brian Landes <the.brian.landes@gmail.com>
2024-03-09 12:57:03 -0700

    List input and custom separator on ConcatStringsNode

--- Assets/Narramancer/Scripts/Nodes/ConcatStringsNode.cs
+++ Assets/Narramancer/Scripts/Nodes/ConcatStringsNode.cs
@@ -5,10 +5,31 @@ using XNode;
 namespace Narramancer {
 	public class ConcatStringsNode : Node {
 
+		[Input(ShowBackingValue.Never, ConnectionType.Multiple, TypeConstraint.Strict)]
+		[SerializeField]
+		private List<string> list = default;
+
 
 		[Input(ShowBackingValue.Never, ConnectionType.Multiple, TypeConstraint.Strict)]
 		[SerializeField]
 		private string elements = "";
+
+		public enum SeparatorType {
+			Comma,
+			NewLine,
+			Tab,
+			Custom
+		}
+
+		[NodeEnum]
+		[SerializeField]
+		private SeparatorType separatorType = SeparatorType.Comma;
+
+		//TODO: hide when type is not Custom
+		[Input(ShowBackingValue.Unconnected, ConnectionType.Override, TypeConstraint.Strict)]
+		[SerializeField]
+		private string separator = ", ";
+
 
 		[Output(ShowBackingValue.Never, ConnectionType.Multiple, TypeConstraint.Strict)]
 		[SerializeField]
@@ -16,8 +37,35 @@ namespace Narramancer {
 
 		public override object GetValue(IDictionary<string, object> context, NodePort port) {
 			if (Application.isPlaying && port.fieldName.Equals(nameof(result))) {
+
+				var values = new List<string>();
+
+				var list = this.GetInputValueList<string>(context, nameof(this.list));
+				if (list != null) {
+					values.AddRange(list.WithoutNulls());
+				}
+
 				var inputValues = GetInputValues<string>(context, nameof(elements));
-				return inputValues.CommaSeparated();
+				values.AddRange(inputValues.WithoutNulls());
+
+				var separator = GetInputValue(context, nameof(this.separator), this.separator);
+
+				switch (separatorType) {
+					case SeparatorType.Comma:
+						separator = ", ";
+						break;
+					case SeparatorType.NewLine:
+						separator = "\n";
+						break;
+					case SeparatorType.Tab:
+						separator = "\t";
+						break;
+					case SeparatorType.Custom:
+						// leave it as is
+						break;
+				}
+
+				return string.Join(separator, values);
 			}
 			return null;
 		}
