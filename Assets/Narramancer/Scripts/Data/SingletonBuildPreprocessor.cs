4cad11f8060cbcd9dcc27325347a82a5aee886e6
Brian Landes <the.brian.landes@gmail.com>
2024-03-27 09:50:45 -0600

    Update SingletonBuildPreprocessor.cs

--- Assets/Narramancer/Scripts/Data/SingletonBuildPreprocessor.cs
+++ Assets/Narramancer/Scripts/Data/SingletonBuildPreprocessor.cs
@@ -10,6 +10,13 @@ namespace Narramancer {
 		public int callbackOrder => 0;
 
 		public void OnPreprocessBuild(BuildReport report) {
+
+			EditorUtility.DisplayDialog("Upgrade Narramancer", "Please upgrade to the full version of Narramancer in order to build a release.", "OK");
+
+			Application.OpenURL("https://assetstore.unity.com/packages/tools/visual-scripting/narramancer-269301");
+
+			Debug.LogException(new BuildFailedException("Please upgrade to the full version of Narramancer (https://assetstore.unity.com/packages/tools/visual-scripting/narramancer-269301) in order to build a release."));
+
 			var singletons = Resources.LoadAll<SingletonBase>(string.Empty);
 
 			foreach( var singleton in singletons) {
